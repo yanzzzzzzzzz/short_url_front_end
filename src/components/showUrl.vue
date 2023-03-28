0537da5ccadd5d506d18451399eb8e70335f5999
Jack Tsai <jtsai@nexify.com.hk>
2023-03-28 16:17:24 +0800

    Add show url component

---
+++ src/components/showUrl.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="row bg-white text-black rounded my-3 custom-class"
+    v-for="(url, shortUrl) in urlMap"
+    :key="url"
+    style="text-align: left; height: 52px"
+  >
+    <div class="col-5">
+      {{ url.substr(8, 25) }}
+    </div>
+    <div class="col-4">
+      <a :href="shortUrl" target="_blank">{{
+        shortUrl.replace("http://localhost:", "")
+      }}</a>
+    </div>
+    <div class="col-1">Copy</div>
+    <div class="col-2">Delete</div>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    urlMap: {
+      required: true,
+      type: Array,
+    },
+  },
+  methods: {},
+};
+</script>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -1,8 +1,8 @@
 <template>
-  <div class="container">
+  <div class="container pt-2" style="max-width: 550px; min-height: 300px">
     <div class="row">
       <div class="col">
-        <div>Free URL Shortener</div>
+        <h1>Free URL Shortener</h1>
       </div>
     </div>
     <div class="row justify-content-center">
@@ -20,15 +20,38 @@
         </button>
       </div>
     </div>
+    <showUrl :urlMap="urlMap" />
+  </div>
+  <div
+    class="text-black"
+    style="background-color: #f5f7f8; min-height: 300px; padding: 100px 300px"
+  >
+    <h2>Squeeze more into every link.</h2>
+    <div>
+      With URLSqueezer, you can turn long and unwieldy URLs into short, snappy
+      links that are easy to share, remember, and use. Our powerful compression
+      technology lets you fit more information into every link, making it the
+      perfect tool for social media, email marketing, and any other situation
+      where space is at a premium. So why settle for long and complicated URLs
+      when you can squeeze more into every link with URLSqueezer?
+    </div>
   </div>
 </template>
 
 <script>
 import urlService from "../service/url";
+import showUrl from "../components/showUrl.vue";
 export default {
+  components: {
+    showUrl,
+  },
   data() {
     return {
       url: "",
+      urlMap: {
+        "https://goo.gl": "https://www.google.com",
+        "https://fb.me": "https://www.facebook.com",
+      },
     };
   },
   methods: {
@@ -36,8 +59,46 @@ export default {
       console.log("generateUrl", this.url);
       urlService.getShortenUrl(this.url).then((data) => {
         console.log("data", data);
+        this.addUrl(this.url, data.shortUrl);
       });
+    },
+    addUrl(url, shortUrl) {
+      this.urlMap[shortUrl] = url;
     },
   },
 };
 </script>
+<style>
+.custom-class {
+  display: flex;
+  align-items: center;
+  height: 200px;
+}
+.cross {
+  display: inline-block;
+  width: 20px;
+  height: 20px;
+  line-height: 20px;
+  text-align: center;
+  background-color: #fff;
+  border: 1px solid #000;
+  color: #000;
+}
+
+.cross:before,
+.cross:after {
+  content: "";
+  position: absolute;
+  width: 12px;
+  height: 1px;
+  background-color: #000;
+}
+
+.cross:before {
+  transform: rotate(45deg);
+}
+
+.cross:after {
+  transform: rotate(-45deg);
+}
+</style>
