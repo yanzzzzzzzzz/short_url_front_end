39f0f06cc743bd8acc5c993e85d4d2b0018894b4
Jack Tsai <jtsai@nexify.com.hk>
2023-06-16 18:00:42 +0800

    process sourcec url too long

--- src/components/showUrl.vue
+++ src/components/showUrl.vue
@@ -6,7 +6,9 @@
     style="text-align: left; height: 52px"
   >
     <div class="col-5">
-      {{ url.originUrl.replace(/^(https?:\/\/)?/, "") }}
+      {{
+        url.originUrl.replace(/^(https?:\/\/)?/, '').slice(0, maxLength) + '...'
+      }}
     </div>
     <div class="col-5">
       <a :href="AddRedirectUrl(url.shortUrl)" target="_blank">{{
@@ -33,22 +35,27 @@
 </template>
 <script>
 export default {
-  emits: ["deleteUrl"],
+  emits: ['deleteUrl'],
   props: {
     urlMap: {
       required: true,
       type: Object,
     },
   },
+  data() {
+    return {
+      maxLength: 20,
+    };
+  },
   methods: {
     copyUrl(url) {
       navigator.clipboard.writeText(url).then(() => {
-        console.log("copy success");
-        alert("Success", "copy success.");
+        console.log('copy success');
+        alert('Success', 'copy success.');
       });
     },
     deleteUrl(urlObj) {
-      this.$emit("deleteUrl", urlObj);
+      this.$emit('deleteUrl', urlObj);
     },
     AddRedirectUrl(shortUrl) {
       return `${this.$root.$el.baseURI}api/url/${shortUrl}`;
