4f46edb135fc56feaf439d5bedacc47591d2df32
Jack Tsai <jtsai@nexify.com.hk>
2023-07-21 17:36:33 +0800

    update test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -3,6 +3,8 @@ describe('login', () => {
     cy.visit('http://127.0.0.1:5173/');
     cy.contains('Login').click();
     cy.url().should('include', '/login');
+    cy.get('[data-cy="username"]').type('mluukkai');
+    cy.get('[data-cy="password"]').type('salainen');
     cy.get('[data-cy="submit"]').click();
     cy.contains('Logout').click();
   });
