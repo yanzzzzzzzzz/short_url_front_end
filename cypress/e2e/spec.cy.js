a43c48206797486d5c89c27a43ff1224fecf9c56
Jack Tsai <jtsai@nexify.com.hk>
2023-07-21 18:00:51 +0800

    test short url

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,8 +1,9 @@
 describe('test', () => {
-  it('Successfully logs in and logs out', () => {
+  beforeEach(() => {
     // Visit the website's homepage
     cy.visit('http://127.0.0.1:5173/');
-
+  });
+  it('Successfully logs in and logs out', () => {
     // Click on the "Login" button, should redirect to the login page
     cy.contains('Login').click();
     cy.url().should('include', '/login');
@@ -15,5 +16,9 @@ describe('test', () => {
     // After successful login, it should redirect to the user page or any other success page
     // Assuming that after login, the "Logout" button will be displayed, so check if it exists
     cy.contains('Logout').click();
+  });
+  it('test short url', () => {
+    cy.get('[data-cy="url"]').type('https://www.google.com.tw/');
+    cy.get('[data-cy="shortUrl"]').click();
   });
 });
