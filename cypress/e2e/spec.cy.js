7dfa5f4e98672b94cf52a9e250b32eadb2823c78
Jack Tsai <jtsai@nexify.com.hk>
2023-07-21 17:17:46 +0800

    add test

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,15 @@
-describe('template spec', () => {
+describe('login', () => {
   it('passes', () => {
     cy.visit('http://127.0.0.1:5173/');
+    cy.contains('Login').click();
+    cy.url().should('include', '/login');
+    cy.get('[data-cy="submit"]').click();
+    cy.contains('Logout').click();
   });
 });
+// describe('My First Test', () => {
+//   it('finds the content "hype"', () => {
+//     cy.visit('https://example.cypress.io');
+//     cy.contains('thype');
+//   });
+// });
