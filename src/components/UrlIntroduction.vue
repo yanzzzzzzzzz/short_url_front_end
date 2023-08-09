f3a6f9a701a1c9a1b69acf728a0dc7af13f14419
Jack Tsai <jtsai@nexify.com.hk>
2023-08-09 17:21:57 +0800

    Refactor: introduction component

---
+++ src/components/UrlIntroduction.vue
@@ -0,0 +1,16 @@
+<template>
+  <div
+    class="text-black"
+    style="background-color: #f5f7f8; min-height: 300px; padding: 100px 300px"
+  >
+    <h2>Squeeze more into every link.</h2>
+    <div>
+      With URLSqueezer, you can turn long and unwieldy URLs into short, snappy
+      links that are easy to share, remember, and use. Our powerful compression
+      technology lets you fit more information into every link, making it the
+      perfect tool for social media, email marketing, and any other situation
+      where space is at a premium. So why settle for long and complicated URLs
+      when you can squeeze more into every link with URLSqueezer?
+    </div>
+  </div>
+</template>
--- src/view/Home.vue
+++ src/view/Home.vue
@@ -4,20 +4,7 @@
     <UrlInputForm v-model="url" @generateUrl="generateUrl" />
     <ShowUrl :urlMap="urls" @deleteUrl="deleteUrl" />
   </div>
-  <div
-    class="text-black"
-    style="background-color: #f5f7f8; min-height: 300px; padding: 100px 300px"
-  >
-    <h2>Squeeze more into every link.</h2>
-    <div>
-      With URLSqueezer, you can turn long and unwieldy URLs into short, snappy
-      links that are easy to share, remember, and use. Our powerful compression
-      technology lets you fit more information into every link, making it the
-      perfect tool for social media, email marketing, and any other situation
-      where space is at a premium. So why settle for long and complicated URLs
-      when you can squeeze more into every link with URLSqueezer?
-    </div>
-  </div>
+  <UrlIntroduction />
 </template>
 
 <script>
@@ -25,11 +12,13 @@ import urlService from '../service/url';
 import ShowUrl from '../components/showUrl.vue';
 import UrlInputForm from '../components/UrlInputForm.vue';
 import UrlHeader from '../components/UrlHeader.vue';
+import UrlIntroduction from '../components/UrlIntroduction.vue';
 export default {
   components: {
     ShowUrl,
     UrlInputForm,
     UrlHeader,
+    UrlIntroduction,
   },
   data() {
     return {
