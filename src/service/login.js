e373a4ded380b8e19681ee5ceedca851defe0626
Jack Tsai <jtsai@nexify.com.hk>
2023-09-27 16:48:30 +0800

    Fix login service return value

--- src/service/login.js
+++ src/service/login.js
@@ -1,9 +1,9 @@
-import axios from "axios";
-const baseURL = "/api/login";
+import axios from 'axios';
+const baseURL = '/api/login';
 
 const login = async (credentials) => {
   const response = await axios.post(baseURL, credentials);
-  return response.data;
+  return response;
 };
 
 export default { login };
