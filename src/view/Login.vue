21c31c77873dd2033765a2b7e85a03db6dcc6293
Jack Tsai <jtsai@nexify.com.hk>
2024-03-14 11:40:46 +0800

    Refactor to Vue3 composition API

--- src/view/Login.vue
+++ src/view/Login.vue
@@ -26,38 +26,41 @@
   </div>
 </template>
 
-<script>
+<script setup lang="ts">
 import loginService from '../service/login';
 import urlService from '../service/url';
-export default {
-  data() {
-    return {
-      username: '',
-      password: '',
-      errorMessage: '',
-    };
-  },
-  methods: {
-    async login() {
-      if (this.username && this.password) {
-        const { data } = await loginService.login({
-          username: this.username,
-          password: this.password,
+import { ref } from 'vue';
+import router from '../router';
+import store from '../store';
+const username = ref('');
+const password = ref('');
+const errorMessage = ref('');
+
+const login = async () => {
+  if (username.value && password.value) {
+    try {
+      const { data } = await loginService.login({
+        username: username.value,
+        password: password.value,
+      });
+      console.log('data', data);
+
+      if (data.token != null) {
+        urlService.setToken(data.token);
+        store.dispatch('setUser', {
+          token: data.token,
+          username: data.username,
+          name: data.name,
         });
-        if (data.token) {
-          urlService.setToken(data.token);
-          this.$store.dispatch('setUser', {
-            token: data.token,
-            username: data.username,
-            name: data.name,
-          });
-          this.$router.push({ name: 'home' });
-        }
-      } else {
-        this.errorMessage = 'Please enter both username and password.';
+
+        router.push({ name: 'home' });
       }
-    },
-  },
+    } catch (error) {
+      errorMessage.value = 'Login failed. Please check your credentials.';
+    }
+  } else {
+    errorMessage.value = 'Please enter both username and password.';
+  }
 };
 </script>
 <style>
