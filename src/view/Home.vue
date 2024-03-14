a006256fd499775ab91af62a73c838bd16440a6c
Jack Tsai <jtsai@nexify.com.hk>
2024-03-14 18:01:53 +0800

    Refactor to vue3 composition API

--- src/view/Home.vue
+++ src/view/Home.vue
@@ -7,63 +7,48 @@
   <UrlIntroduction />
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref, onMounted } from 'vue';
 import urlService from '../service/url';
 import ShowUrl from '../components/showUrl.vue';
 import UrlInputForm from '../components/UrlInputForm.vue';
 import UrlHeader from '../components/UrlHeader.vue';
 import UrlIntroduction from '../components/UrlIntroduction.vue';
-export default {
-  components: {
-    ShowUrl,
-    UrlInputForm,
-    UrlHeader,
-    UrlIntroduction,
-  },
-  data() {
-    return {
-      url: '',
-      urls: [],
-    };
-  },
-  async created() {
-    if (this.$store.state.user.token == null) {
-      var localStorageUrlList = this.getShortUrlList();
-      this.urls =
-        localStorageUrlList !== null ? JSON.parse(localStorageUrlList) : [];
-    } else {
-      const { data: urls } = await urlService.getAllUrl();
-      this.urls = urls;
-    }
-  },
-  methods: {
-    async generateUrl() {
-      const data = await urlService.createShortUrl(this.url);
-      this.addUrl(this.url, data.shortUrl);
-      this.url = '';
-    },
-    addUrl(url, shortUrl) {
-      this.urls = this.urls.concat({
-        originUrl: url,
-        shortUrl: `${shortUrl}`,
-      });
-      if (this.$store.state.user.token == null) {
-        this.saveShortUrlList(JSON.stringify(this.urls));
-      }
-    },
-    async deleteUrl(urlObj) {
-      this.urls = this.urls.filter((url) => url.shortUrl !== urlObj.shortUrl);
-      const response = await urlService.deleteUrl(urlObj.shortUrl);
-      if (this.$store.state.token == null) {
-        this.saveShortUrlList(JSON.stringify(this.urls));
-      }
-    },
-    saveShortUrlList(shortUrl) {
-      localStorage.setItem('shortUrl', shortUrl);
-    },
-    getShortUrlList() {
-      return localStorage.getItem('shortUrl');
-    },
-  },
+import store from '../store';
+
+const url = ref('');
+const urls = ref<Array<{ originUrl: string; shortUrl: string }>>([]);
+
+const addUrl = (url: string, shortUrl: string): void => {
+  urls.value = [...urls.value, { originUrl: url, shortUrl }];
+};
+
+const deleteUrl = async (urlObj: {
+  originUrl: string;
+  shortUrl: string;
+}): Promise<void> => {
+  urls.value = urls.value.filter((u) => u.shortUrl !== urlObj.shortUrl);
+  const response = await urlService.deleteUrl(urlObj.shortUrl);
+};
+
+const generateUrl = async (): Promise<void> => {
+  const data = await urlService.createShortUrl(url.value);
+  addUrl(url.value, data.shortUrl);
+};
+
+onMounted(async () => {
+  console.log('on mounted', store.state.user.token);
+  if (store.state.user.token || localStorage.getItem('token')) {
+    const { data } = await urlService.getAllUrl();
+
+    urls.value = data.map((item) => ({
+      originUrl: item.originUrl,
+      shortUrl: item.shortUrl,
+    }));
+  }
+});
+
+const getShortUrlList = (): string | null => {
+  return localStorage.getItem('shortUrl');
 };
 </script>
