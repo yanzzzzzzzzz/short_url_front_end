161ce0951fd1c23f94b4b5531e66130225fdd9bc
Jack Tsai <jtsai@nexify.com.hk>
2024-04-12 17:22:29 +0800

    format

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,15 @@
-import { defineConfig } from "vite";
-import vue from "@vitejs/plugin-vue";
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
   server: {
     proxy: {
-      "/api": {
-        target: "http://localhost:4001",
-        changeOrigin: true,
-      },
-    },
-  },
+      '/api': {
+        target: 'http://localhost:4001',
+        changeOrigin: true
+      }
+    }
+  }
 });
