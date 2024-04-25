5603e4b9200f677c2b11f90960ab8c21d1670770
Jack Tsai <jtsai@nexify.com.hk>
2024-04-25 17:57:06 +0800

    Refactor rename

--- src/utils/googleLogin.ts
+++ src/utils/googleLogin.ts
@@ -2,33 +2,28 @@ function getGoogleOAuthURL() {
   const rootUrl = 'https://accounts.google.com/o/oauth2/v2/auth';
 
   const config = {
-    NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URL: "http://localhost:4001/api/sessions/oauth/google",
-    NEXT_PUBLIC_GOOGLE_CLIENT_ID: "393147601550-fikm3621o6j5ub41cntmnp3clh9uhhjf.apps.googleusercontent.com"
+    GoogleOAuthRedirectUrl: "http://localhost:4001/api/sessions/oauth/google",
+    GoogleClientId: "393147601550-fikm3621o6j5ub41cntmnp3clh9uhhjf.apps.googleusercontent.com"
   }
   const options: {
-    redirect_uri?: string;
-    client_id: string;
-    access_type: string;
-    response_type: string;
+    redirectUri?: string;
+    clientId: string;
+    accessType: string;
+    responseType: string;
     prompt: string;
     scope: string;
   } = {
-    redirect_uri: config.NEXT_PUBLIC_GOOGLE_OAUTH_REDIRECT_URL,
-    client_id: config.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
-    access_type: 'offline',
-    response_type: 'code',
+    redirectUri: config.GoogleOAuthRedirectUrl,
+    clientId: config.GoogleClientId as string,
+    accessType: 'offline',
+    responseType: 'code',
     prompt: 'consent',
     scope: [
       'https://www.googleapis.com/auth/userinfo.profile',
       'https://www.googleapis.com/auth/userinfo.email'
     ].join(' ')
   };
-  console.log({ options });
-
   const qs = new URLSearchParams(options);
-
-  console.log({ qs });
-
   return `${rootUrl}?${qs.toString()}`;
 }
 
