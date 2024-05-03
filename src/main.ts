638c7b7f3b2a681ea223ba659011d2c6ddbcefe7
Jack Tsai <jtsai@nexify.com.hk>
2024-05-03 17:31:19 +0800

    Change theme

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
 import { createPinia } from 'pinia';
 import PrimeVue from 'primevue/config';
 import "primeflex/primeflex.css";
-import 'primevue/resources/themes/saga-blue/theme.css';
+import 'primevue/resources/themes/aura-light-green/theme.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
 
