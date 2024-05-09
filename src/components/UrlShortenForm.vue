118ceb24b85fe7d70bdab48171be463990a0aa04
Jack Tsai <jtsai@nexify.com.hk>
2024-05-09 17:39:10 +0800

    Update UrlShortenForm style

--- src/components/UrlShortenForm.vue
+++ src/components/UrlShortenForm.vue
@@ -1,20 +1,27 @@
 <template>
   <div class="shortUrlForm">
     <p>Shorten a long URL</p>
-    <input type="text" placeholder="Enter long link here" v-model="url" data-cy="urlInputText" />
+    <InputText
+      class="w-full"
+      placeholder="Enter long link here"
+      v-model="url"
+      data-cy="urlInputText"
+    />
     <p>Customize your link(optional)</p>
-    <input type="text" placeholder="Enter alias" v-model="customShortUrl" />
-    <button type="button" class="btn btn-primary" @click="generateUrl" data-cy="shortUrl">
-      Shorten URL
-    </button>
+    <InputText class="w-full" placeholder="Enter alias" v-model="customShortUrl" />
     <p style="color: red">{{ messageStore.getErrorMessage }}</p>
+    <Button class="mt-4 block" @click="generateUrl" data-cy="shortUrl" label="Shorten URL" />
   </div>
 </template>
 <script setup lang="ts">
 import { useMessageStore } from '../stores/MessageStore';
+import Button from 'primevue/button';
+import InputText from 'primevue/inputtext';
+import { Ref } from 'vue';
+
 const messageStore = useMessageStore();
-const url = defineModel('url');
-const customShortUrl = defineModel('customShortUrl');
+const url = defineModel('url') as Ref<string | undefined>;
+const customShortUrl = defineModel('customShortUrl') as Ref<string | undefined>;
 const emits = defineEmits(['generateUrl']);
 const generateUrl = () => {
   emits('generateUrl');
@@ -31,7 +38,7 @@ const generateUrl = () => {
   border-radius: 0.6rem;
   margin-bottom: 1rem;
 }
-input[type='text'] {
+/* input[type='text'] {
   width: 100%;
   padding: 10px;
   margin-bottom: 20px;
@@ -47,5 +54,5 @@ input[type='text']::placeholder {
 }
 input[type='text']:focus {
   border-color: #ccc;
-}
+} */
 </style>
